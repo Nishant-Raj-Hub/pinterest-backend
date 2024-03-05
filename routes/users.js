f59e85dcf04a25a0a8493df11bb51bba7a30482d
Nishant-Raj-Hub <nishantraj2109@gmail.com>
2024-03-05 17:33:44 +0530

    test

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/pinterest-clone");
+mongoose.connect('mongodb+srv://noddy:noddy@cluster0.zwio6op.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 const userSchema = new mongoose.Schema({
   username: {
