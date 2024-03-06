3d77048c5dec87a55b5cd1071ea38dcaaffeb798
Nishant Raj <nishantraj2109@gmail.com>
2024-03-06 21:53:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var createError = require('http-errors');
 var express = require('express');
 
 const expressSession = require('express-session');
-const flash = require("connect-flash");
+// const flash = require("connect-flash");
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -27,7 +27,7 @@ mongoose.connect('mongodb+srv://noddy:noddy@cluster0.zwio6op.mongodb.net/', {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(flash());
+// app.use(flash());
 app.use(expressSession({
   resave: false,
   saveUninitialized: false,
@@ -40,7 +40,7 @@ passport.deserializeUser(usersRouter.deserializeUser());
 
 
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -55,14 +55,14 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
